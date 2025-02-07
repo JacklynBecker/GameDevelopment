577c3e419babc2c5e165d69fb7360caa88a8dead
JacklynBecker <200626295@student.georgianc.on.ca>
2025-02-07 11:25:21 -0500

    added colour chnage to paddles when they are hit

--- Assets/scripts/UiManager.cs
+++ Assets/scripts/UiManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aebf4253e5f9ed182055d1e94dc6b182d0b0502bd325211b250d2b919b16dc66
-size 1411
+oid sha256:593ed1b732bc851d53fe452a7396ee04a22c06a93bdcd36b5bd0f2d670f7e17a
+size 1827
