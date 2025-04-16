6e0f3d9fe36e68b494b8a648c00bc48c4f98f69d
JacklynBecker <200626295@student.georgianc.on.ca>
2025-04-15 23:05:03 -0400

    added player and background

---
+++ FinalExam/SnowBallRun/Assets/scripts/BallMovement.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:acdd9ad9d5d88f4febf7cec643cdc2b50efacc49b7ff65f034d79b073c1e4787
+size 440
---
+++ FinalExam/SnowBallRun/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7737d8e24fc4bd0dc48c29c5edd00af07078311c4fabbb577d8918fdd20544ef
+size 556
